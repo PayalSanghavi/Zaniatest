85ed87599d742de23e6aed8522d868c4e2e7cec0
PayalSanghavi <mspayalsanghavi@gmail.com>
2024-08-22 18:31:05 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
+useEffect(() => {
+  const handleEsc = (event) => {
+    if (event.key === 'Escape') {
+      handleOverlayClose();
+    }
+  };
+
+  window.addEventListener('keydown', handleEsc);
+  return () => {
+    window.removeEventListener('keydown', handleEsc);
+  };
+}, []);
 
