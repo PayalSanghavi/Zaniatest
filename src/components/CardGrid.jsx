0b44209c1fe0a24f36d0025401fd33f3712db8d1
PayalSanghavi <mspayalsanghavi@gmail.com>
2024-08-22 18:06:02 +0530

    Update CardGrid.jsx

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -2,6 +2,47 @@ import React, { useEffect, useState } from 'react';
 import { useDrag, useDrop } from 'react-dnd';
 import { ItemTypes } from './ItemTypes';
 
+import Spinner from './Spinner'; // Create a simple spinner component
+
+const Card = ({ card, index, moveCard, onClick }) => {
+  const [loading, setLoading] = useState(true);
+
+  const handleImageLoad = () => setLoading(false);
+
+  return (
+    <div
+      ref={drag}
+      style={{
+        position: 'relative',
+        width: '200px',
+        margin: '10px',
+        display: 'inline-block',
+        cursor: 'move',
+      }}
+      onClick={() => onClick(card)}
+    >
+      {loading && <Spinner />}
+      <img
+        src={card.thumbnail}
+        alt={card.title}
+        onLoad={handleImageLoad}
+        style={{ width: '100%', display: loading ? 'none' : 'block' }}
+      />
+      <div style={{
+        position: 'absolute',
+        bottom: '0',
+        left: '0',
+        width: '100%',
+        background: 'rgba(0,0,0,0.5)',
+        color: 'white',
+        textAlign: 'center'
+      }}>
+        {card.title}
+      </div>
+    </div>
+  );
+};
+
 const Card = ({ card, index, moveCard, onClick }) => {
   const [{ isDragging }, drag] = useDrag({
     type: ItemTypes.CARD,
@@ -98,6 +139,10 @@ const CardGrid = () => {
       </div>
     </div>
   );
+};
+
+export const ItemTypes = {
+  CARD: 'card',
 };
 
 export default CardGrid;
